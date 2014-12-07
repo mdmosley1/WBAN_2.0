7f5d6012ab3ce66e9236eed64f6f3c0a00587164
Matthew Mosley <mdmosley1@gmail.com>
2014-12-06 22:32:38 -0600

    closed #38. App now updates plot title and range label based on current selected sensor

--- src/ti/android/ble/sensortag/DeviceActivity.java
+++ src/ti/android/ble/sensortag/DeviceActivity.java
@@ -288,7 +288,16 @@ public class DeviceActivity extends Activity implements OnItemSelectedListener{
 			int pos, long id) {
 		// An item was selected. You can retrieve the selected item using
 		// parent.getItemAtPosition(pos)
-		selected = parent.getItemAtPosition(pos).toString();		
+		selected = parent.getItemAtPosition(pos).toString();
+
+		if (selected.equals("Accelerometer")){
+			RTPlot.setTitle("Accelerometer");
+			RTPlot.setRangeLabel("Acceleration (G)");
+		}
+		else if (selected.equals("Gyroscope")){
+			RTPlot.setTitle("Gyroscope");
+			RTPlot.setRangeLabel("Deg/s");
+		}
 	}
 
 	public void onNothingSelected(AdapterView<?> parent) {
